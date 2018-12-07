0b56f4e9c0be62bbb2a696eaf18af3a4a09477f0
Anand Varkey Philips <anandvarkeyphilips@gmail.com>
2018-12-08 00:30:50 +0530

    Application Name-Automatic Property Expansion with Spring Boot

--- src/main/java/io/exnihilo/validator/EnterpriseValidator.java
+++ src/main/java/io/exnihilo/validator/EnterpriseValidator.java
@@ -1,6 +1,7 @@
 package io.exnihilo.validator;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.ApplicationPidFileWriter;
@@ -16,27 +17,33 @@ import java.time.format.DateTimeFormatter;
  * There are lots of customizations available.
  *
  * @author Anand Varkey Philips
- * @since  2.0.6.RELEASE
+ * @since 2.0.6.RELEASE
  */
 @EnableSwagger2
 @Slf4j
 @SpringBootApplication
 public class EnterpriseValidator {
 
+    private static String applicationName;
     private static final ZoneId ZONE_ID = ZoneId.of("Asia/Kolkata");
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd:MMM:YYY hh:mm:ss:SSS a");
+
+    @Value("${application.name}")
+    public void setApplicationName(String applicationName) {
+        this.applicationName = applicationName;
+    }
 
     public static void main(String[] args) {
         SpringApplicationBuilder app = new SpringApplicationBuilder(EnterpriseValidator.class);
         app.build().addListeners(new ApplicationPidFileWriter());
         app.run();
-        log.info("Enterprise Validator Application is started at Indian Standard Time: {}",
+        log.info("{} Application is started at Indian Standard Time: {}", applicationName,
                 ZonedDateTime.now(ZONE_ID).format(DATE_TIME_FORMATTER));
     }
 
     @PreDestroy
     void logExit() {
-        log.info("Enterprise Validator Application is being gracefully stopped at Indian Standard Time: {}",
+        log.info("{} Application is being gracefully stopped at Indian Standard Time: {}", applicationName,
                 ZonedDateTime.now(ZONE_ID).format(DATE_TIME_FORMATTER));
     }
 }
