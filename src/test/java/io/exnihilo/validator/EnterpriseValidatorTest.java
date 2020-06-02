978035da8b5cfeb9dbbd2f12245b21713cb6962d
Anand Varkey Philips <anandvarkeyphilips@gmail.com>
2020-06-02 18:48:18 +0530

    refactoring package and adding test yaml files

---
+++ src/test/java/io/exnihilo/validator/EnterpriseValidatorTest.java
@@ -0,0 +1,23 @@
+package io.exnihilo.validator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * This class tests the bean context configurations
+ *
+ * @author Anand Varkey Philips
+ * @since 2.0.6.RELEASE
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnterpriseValidatorTest {
+
+    @Test
+    public void contextLoads() {
+        Assert.assertTrue(true);
+    }
+}
