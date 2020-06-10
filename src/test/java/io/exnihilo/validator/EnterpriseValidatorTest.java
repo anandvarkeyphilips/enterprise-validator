7dcc7088a4c9ea5dc643d4601c2a90b5e7b92906
Anand Varkey Philips <anandvarkeyphilips@gmail.com>
2020-06-11 00:42:59 +0530

    Sonar fixes for log, assertions

--- src/test/java/io/exnihilo/validator/EnterpriseValidatorTest.java
+++ src/test/java/io/exnihilo/validator/EnterpriseValidatorTest.java
@@ -1,8 +1,15 @@
 package io.exnihilo.validator;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import io.exnihilo.validator.config.MemoryAppender;
+import org.assertj.core.api.Assertions;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,13 +23,40 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class EnterpriseValidatorTest {
 
+  private MemoryAppender memoryAppender;
+  private static final String LOGGER_NAME = "io.exnihilo.validator";
+
+  @Before
+  public void setup() {
+    Logger logger = (Logger) LoggerFactory.getLogger(LOGGER_NAME);
+    memoryAppender = new MemoryAppender();
+    memoryAppender.setContext((LoggerContext) LoggerFactory.getILoggerFactory());
+    logger.setLevel(Level.DEBUG);
+    logger.addAppender(memoryAppender);
+    memoryAppender.start();
+  }
+
   @Test
   public void contextLoads() {
     Assert.assertTrue(true);
   }
 
   @Test
-  public void applicationStarts() {
-    EnterpriseValidator.main(new String[] {});
+  public void testApplicationStartAndExitLogs() {
+    EnterpriseValidator enterpriseValidator = new EnterpriseValidator();
+    enterpriseValidator.logApplicationStart();
+    enterpriseValidator.logApplicationExit();
+
+    Assertions.assertThat(memoryAppender.countEventsForLogger(LOGGER_NAME)).isEqualTo(2);
+    Assertions.assertThat(memoryAppender.search("Application is started at Indian Standard Time", Level.INFO).size())
+        .isEqualTo(1);
+    Assertions
+        .assertThat(
+            memoryAppender.search("Application is being gracefully stopped at Indian Standard Time", Level.INFO).size())
+        .isEqualTo(1);
+    Assertions
+        .assertThat(
+            memoryAppender.contains("Application is being gracefully stopped at Indian Standard Time", Level.TRACE))
+        .isFalse();
   }
 }
